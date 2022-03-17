d279991382e84293ba3eaf78468b4d5d707e61de
Diego <diegoarche@hotmail.com>
2022-03-17 06:25:25 +0100

    Capitulo 126: optimización peticiones HTTP

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styles: [
+  styles: [`
+  button{
+    margin-right: 5px;
+    margin-bottom:5px;
+  }`
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent{
+//! ya no vale
+  // regions:string[]=["africa","americas","asia","europe","oceania"];
 
-  constructor() { }
+  regions:string[]=['EU','EFTA','CARICOM','PA','AU','USAN','EEU','AL','ASEAN','CAIS','CEFTA','NAFTA','SAARC'];
 
-  ngOnInit(): void {
+  regionActiva:string="";
+  paises : Country [] = [];
+  constructor(private paisService: PaisService) { }
+
+  getClaseCSS(region:string):string{
+    return (region===this.regionActiva)? 'btn btn-primary' : 'btn btn-outline-primary';
+  }
+  activarRegion(region:string){
+    if(region===this.regionActiva){return}
+    this.paises=[];
+    this.regionActiva=region;
+ this.paisService.buscarRegion(region).subscribe((paises)=>{
+  this.paises=paises;
+ })
   }
 
 }
